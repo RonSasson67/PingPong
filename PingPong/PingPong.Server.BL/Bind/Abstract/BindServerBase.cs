4e42b51e49e524a459db06c1ed0531cd7eb6ba57
RonSasson67 <ron6764527@gmail.com>
2022-01-19 17:33:33 +0200

    create bindbase and clientHandlerBase

---
+++ PingPong/PingPong.Server.BL/Bind/Abstract/BindServerBase.cs
@@ -0,0 +1,23 @@
+﻿using PingPong.Server.BL.ClientHandler.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingPong.Server.BL.Bind.Abstract
+{
+    abstract class BindServerBase
+    {
+        protected ClientHandlerBase _clientHandler;
+        protected int _ip;
+        protected int _port;
+
+        public BindServerBase(ClientHandlerBase clientHandler, int ip, int port)
+        {
+            _clientHandler = clientHandler;
+            _ip = ip;
+            _port = port;
+        }
+
+        public abstract void BindServer();
+    }
+}
---
+++ PingPong/PingPong.Server.BL/ClientHandler/Abstract/ClientHandlerBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Server.BL.ClientHandler.Abstract
+{
+    abstract class ClientHandlerBase
+    {
+        public abstract void RunHandler();
+    }
+}
