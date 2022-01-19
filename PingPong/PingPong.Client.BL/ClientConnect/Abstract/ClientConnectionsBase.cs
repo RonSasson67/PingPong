4ca9beb0a7396f4da135d56932f82c68b1514058
RonSasson67 <ron6764527@gmail.com>
2022-01-19 20:12:59 +0200

    create ClientConnectionBase

---
+++ PingPong/PingPong.Client.BL/ClientConnect/Abstract/ClientConnectionsBase.cs
@@ -0,0 +1,23 @@
+﻿using PingPong.Client.BL.ServerHandler.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingPong.Client.BL.ClientConnect.Abstract
+{
+    abstract class ClientConnectionsBase
+    {
+        protected IServerHandler _serverHandler;
+        protected int _ip;
+        protected int _port;
+
+        public ClientConnectionsBase(IServerHandler clientHandler, int ip, int port)
+        {
+            _serverHandler = clientHandler;
+            _ip = ip;
+            _port = port;
+        }
+
+        public abstract void RunClient();
+    }
+}
