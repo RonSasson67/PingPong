f6921d638c12e5564db799fc7271ed542ebb5b07
RonSasson67 <ron6764527@gmail.com>
2022-01-20 09:09:26 +0200

    create RunClientBase

---
+++ PingPong/PingPong.Client.BL/RunClient/Abstract/RunClientBase.cs
@@ -0,0 +1,17 @@
+﻿using PingPong.Client.BL.ClientConnect;
+using PingPong.Client.BL.ServerHandler.Abstract;
+
+namespace PingPong.Client.BL.RunClient.Abstract
+{
+    public abstract class RunClientBase
+    {
+        private IServerHandler _handler;
+
+        protected RunClientBase(IServerHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public abstract void RunClient();
+    }
+}
