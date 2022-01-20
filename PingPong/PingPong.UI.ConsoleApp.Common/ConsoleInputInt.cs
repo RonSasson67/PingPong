e20bd4a2935f48afde512db0c14d8f0efd3a022c
RonSasson67 <ron6764527@gmail.com>
2022-01-20 09:48:12 +0200

    create Console input output

---
+++ PingPong/PingPong.UI.ConsoleApp.Common/ConsoleInputInt.cs
@@ -0,0 +1,26 @@
+﻿using PingPong.UI.Common;
+using System;
+
+namespace PingPong.UI.ConsoleApp.Common
+{
+    public class ConsoleInputInt : IInput<int>
+    {
+        public int getInput()
+        {
+            do
+            {
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("try input number...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (true);
+        }
+    }
+}
---
+++ PingPong/PingPong.UI.ConsoleApp.Common/ConsoleOutputString.cs
@@ -0,0 +1,15 @@
+﻿using PingPong.UI.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingPong.UI.ConsoleApp.Common
+{
+    public class ConsoleOutputString : IOutput<string>
+    {
+        public void SentOut(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
