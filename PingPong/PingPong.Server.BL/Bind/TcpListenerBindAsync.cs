c8679619bb6ff72c9aa80f27b1f05c4ffda1c5bd
RonSasson67 <ron6764527@gmail.com>
2022-01-20 13:03:13 +0200

    create tcplisent bind

---
+++ PingPong/PingPong.Server.BL/Bind/TcpListenerBindAsync.cs
@@ -0,0 +1,44 @@
+﻿using PingPong.Server.BL.Bind.Abstract;
+using PingPong.Server.BL.ClientHandler.Abstract;
+using PingPong.UI.Common;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Server.BL.Bind
+{
+    public class TcpListenerBindAsync : BindServerBase
+    {
+        IOutput<string> _output;
+        public TcpListenerBindAsync(IClientHandler clientHandler, string ip, int port, IOutput<string> output) : base(clientHandler, ip, port)
+        {
+            _output = output;
+        }
+
+        public override void BindServer()
+        {
+            IPAddress localAddr = IPAddress.Parse(_ip);
+
+            TcpListener server = new TcpListener(localAddr, _port);
+
+            server.Start();
+
+            Appcept(server);
+        }
+
+        private void Appcept(TcpListener server)
+        {
+            while (true)
+            {
+                _output.SentOut("Waiting for a connection... ");
+                
+                TcpClient client = server.AcceptTcpClient();
+                _output.SentOut("Connected!");
+                Task.Run(() => _clientHandler.RunHandler(client));
+            }
+        }
+    }
+}
